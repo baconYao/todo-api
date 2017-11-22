1db0b1eada4393eb84506172f81252192cbf2fea
baconYao <bacon735392@gmail.com>
2017-11-22 09:24:36 +0800

    add delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -37,3 +37,20 @@ exports.getTodosId = (req, res) => {
     res.status(400).send();
   }).catch(e => res.status(404).send());
 };
+
+exports.deleteTodosByID = (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send(); 
+    }
+
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+};
--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,6 @@ const todoController = require("../controllers/todoController");
 router.post('/todos', todoController.postTodos);
 router.get('/todos', todoController.getTodos);
 router.get('/todos/:id', todoController.getTodosId);
+router.delete('/todos/:id', todoController.deleteTodosByID);
 
 module.exports = router;
